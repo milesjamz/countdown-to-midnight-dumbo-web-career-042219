3e676640f8474f74f9c70e6b40b7ed495c1fa3f3
Miles Marillo <mjm03@hampshire.edu>
2019-04-10 16:42:28 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -3,20 +3,22 @@
 def countdown(count)
   count = 10
   while count > 0
+  puts "#{count} SECOND(S)!"
   count -= 1
-  puts "#{count} SECONDS!"
   end
 if count == 0
-puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
+end
 end
 
 def countdown_with_sleep(count)
   count = 10
   while count > 0
-  count -= 1
   puts "#{count} SECONDS!"
-  sleep(1)
+  count -= 1
+  sleep 1
   end
 if count == 0
-puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
+end
 end
