690f4de8325f913cedda49d082339748d573fc5f
Miles Marillo <mjm03@hampshire.edu>
2019-04-10 16:40:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(count)
   count = 10
   while count > 0
   count -= 1
-  puts "#{count} SECONDS!"
+  return "#{count} SECONDS!"
   end
 if count == 0
 return "HAPPY NEW YEAR!"
@@ -15,7 +15,7 @@ def countdown_with_sleep(count)
   count = 10
   while count > 0
   count -= 1
-  puts "#{count} SECONDS!"
+  return "#{count} SECONDS!"
   sleep 1
   end
 if count == 0
